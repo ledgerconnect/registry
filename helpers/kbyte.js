ee1df5300b58a39413769d4914452de0879e5d16
adcpm <fabien@bonustrack.co>
2019-07-20 19:52:57 +0700

    Send heartbeats

--- helpers/kbyte.js
+++ helpers/kbyte.js
@@ -4,4 +4,6 @@ const Promise = require('bluebird');
 Promise.promisifyAll(kbyte.Client.prototype);
 const client = new kbyte.Client('wss://obyte.org/bb');
 
+setInterval(() => client.request('heartbeat', null), 10 * 1000);
+
 module.exports = client;
--- src/obyte.js
+++ src/obyte.js
@@ -5,11 +5,21 @@ let lastState;
 const address = 'FAB6TH7IRAVHDLK2AAWY5YBE6CEBUACF';
 const params = { addresses: [address], last_ball_mci: 1000000000, amount: 1 };
 
-setInterval(() => {
+const check = () => {
   client.requestAsync('light/pick_divisible_coins_for_amount', params).then(result => {
     const state = JSON.stringify(result);
-    if (lastState && lastState === state)
-      sendMessage(`Oops, nothing changed here: <https://explorer.obyte.org/#${address}>`);
+    if (lastState && lastState === state) {
+      console.log('Oops, address is not posting');
+      sendMessage(`Oops, nothing changed! <https://explorer.obyte.org/#${address}>`);
+    } else {
+      console.log('Great, address is posting');
+    }
     lastState = state;
   });
+};
+
+check();
+setInterval(() => {
+  check();
 }, 60 * 60 * 1000);
+
